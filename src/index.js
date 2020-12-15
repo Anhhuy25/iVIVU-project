34e8f3d84e308d2706fa38569c3959f6078320b2
Anhhuy25 <vuducanhhuy@gmail.com>
2020-12-15 14:27:31 +0700

    Update the first time

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import Project1 from './small projects/Project1';
+import { AppProvider } from './components/project 1/context';
+import './components/project 1/index.css'
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  // <App />
+  <AppProvider>
+    <Project1 />
+  </AppProvider>
+  , document.getElementById('root')
 );
 
 
